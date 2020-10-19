222217f73409e8082b7feced01b6e3776f67e9d4
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-10-19 10:08:19 +0700

    disable trailing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9202491ef84bb95e7f11";
+/******/ 	var hotCurrentHash = "8daca059516bf0c3185d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
 module.exports = {
-    trailingSlash: true,
+    trailingSlash: false,
 }
