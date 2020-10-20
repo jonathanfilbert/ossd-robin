bfa4559a7d1eb679bb9f910b47ba0656d8259c69
Leonardo81 <leonardoeinstein2000@gmail.com>
2020-10-20 17:44:25 +0700

    config path mapping

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /* eslint-disable no-undef */
 module.exports = {
     trailingSlash: false,
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          '/': { page: '/' },
+          '/meet': { page: '/meet' },
+          '/create': { page: '/create' },
+        }
+      },
 }
